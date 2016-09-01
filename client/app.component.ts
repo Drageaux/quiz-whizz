1ec6f7b978e7fdc54f61c57be496c98d81ee38c5
David Thong Nguyen <thnguyen2808@gmail.com>
2016-09-01 11:02:36 -0400

    Setting up complete

--- client/app.component.ts
+++ client/app.component.ts
@@ -6,28 +6,28 @@ import { SemanticPopupComponent } from "ng-semantic";
     selector: "app",
     template: `
 <div class="ui container">
-    <nav class="ui menu inverted teal huge">
+    <nav class="ui menu inverted red huge">
         <a routerLink="home" class="item">Home</a>
-        <a routerLink="contact" class="item">Contact Me</a>
+        <!--<a routerLink="contact" class="item">Contact Me</a>-->
         
-        <nav class="menu right">
-            <a (click)="myPopup.show($event, {position: 'right center'})" *ngIf="!isLogged" class="item">Login</a>
-            <a (click)="logout()" *ngIf="isLogged" class="item inverted red">Logout</a>
-        </nav>
+        <!--<nav class="menu right">-->
+            <!--<a (click)="myPopup.show($event, {position: 'right center'})" *ngIf="!isLogged" class="item">Login</a>-->
+            <!--<a (click)="logout()" *ngIf="isLogged" class="item inverted red">Logout</a>-->
+        <!--</nav>-->
     </nav>
 
-    <sm-popup class="huge" #myPopup>
-        <sm-card class="card basic">
-            <card-title> Simple login </card-title>
-            <card-subtitle>  </card-subtitle>
-            <card-content>
-                <p><b>Password</b>: angualr2express</p>
-                <p><b>Username</b>: john</p>
-            </card-content>
-            <sm-button class="bottom attached fluid primary" *ngIf="!isLogged" (click)="login()">Login</sm-button>
-            <sm-button class="bottom attached fluid red" *ngIf="isLogged" (click)="logout()">Logout</sm-button>
-        </sm-card>
-    </sm-popup>
+    <!--<sm-popup class="huge" #myPopup>-->
+        <!--<sm-card class="card basic">-->
+            <!--<card-title> Simple login </card-title>-->
+            <!--<card-subtitle>  </card-subtitle>-->
+            <!--<card-content>-->
+                <!--<p><b>Password</b>: angualr2express</p>-->
+                <!--<p><b>Username</b>: john</p>-->
+            <!--</card-content>-->
+            <!--<sm-button class="bottom attached fluid primary" *ngIf="!isLogged" (click)="login()">Login</sm-button>-->
+            <!--<sm-button class="bottom attached fluid red" *ngIf="isLogged" (click)="logout()">Logout</sm-button>-->
+        <!--</sm-card>-->
+    <!--</sm-popup>-->
         
     <hello [name]="appName"></hello>
     
@@ -35,56 +35,63 @@ import { SemanticPopupComponent } from "ng-semantic";
     
     <router-outlet></router-outlet>
     
-    <div class="center">
-        <img src='https://angular.io/resources/images/logos/standard/shield-large.png'>
-    </div>
-    
 </div>`
 })
 export class AppComponent {
-    appName: string = "Angular 2 Express";
-    user: any = {
+    appName:string = "Angular 2 Express";
+    user:any = {
         password: "angualr2express",
         username: "john"
     };
-    response: Response;
-    isLogged: boolean;
-    @ViewChild("myPopup") myPopup: SemanticPopupComponent;
+    response:Response;
+    isLogged:boolean;
+    @ViewChild("myPopup") myPopup:SemanticPopupComponent;
 
-    constructor(private http: Http) {
+    constructor(private http:Http) {
         this.isLogged = !!localStorage.getItem("id_token");
     }
 
-    signup() {
-        this.http.post("/login/signup", JSON.stringify({ password: this.user.password, username: this.user.username }), new RequestOptions({
-            headers: new Headers({"Content-Type": "application/json"})
-        }))
-            .map((res: any) => res.json())
-            .subscribe(
-                (res: Response) => {
-                    this.response = res;
-                },
-                (error: Error) => { console.log(error); }
-            );
-    }
 
-    login() {
-        this.http.post("/login", JSON.stringify({ password: this.user.password }), new RequestOptions({
-            headers: new Headers({"Content-Type": "application/json"})
-        }))
-            .map((res: Response) => res.json())
-            .subscribe(
-                (res: Response & { jwt: string }) => {
-                    localStorage.setItem("id_token", res.jwt);
-                    this.myPopup.hide();
-                    location.reload();
-                },
-                (error: Error) => { console.log(error); }
-            );
-    }
 
-    logout(): void {
-        localStorage.removeItem("id_token");
-        location.reload();
-    }
+
+
+    //signup() {
+    //    this.http.post("/login/signup", JSON.stringify({
+    //            password: this.user.password,
+    //            username: this.user.username
+    //        }), new RequestOptions({
+    //            headers: new Headers({"Content-Type": "application/json"})
+    //        }))
+    //        .map((res:any) => res.json())
+    //        .subscribe(
+    //            (res:Response) => {
+    //                this.response = res;
+    //            },
+    //            (error:Error) => {
+    //                console.log(error);
+    //            }
+    //        );
+    //}
+    //
+    //login() {
+    //    this.http.post("/login", JSON.stringify({password: this.user.password}), new RequestOptions({
+    //            headers: new Headers({"Content-Type": "application/json"})
+    //        }))
+    //        .map((res:Response) => res.json())
+    //        .subscribe(
+    //            (res:Response & { jwt: string }) => {
+    //                localStorage.setItem("id_token", res.jwt);
+    //                this.myPopup.hide();
+    //                location.reload();
+    //            },
+    //            (error:Error) => {
+    //                console.log(error);
+    //            }
+    //        );
+    //}
+    //
+    //logout():void {
+    //    localStorage.removeItem("id_token");
+    //    location.reload();
+    //}
 }
