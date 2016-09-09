89b37d03bc50d8761dd8a3f3012a5a1356b4ed70
David Thong Nguyen <thnguyen2808@gmail.com>
2016-09-09 13:11:08 -0400

    Basic expression generation in progress
    needs conditions and testing

--- server/routes/quiz.ts
+++ server/routes/quiz.ts
@@ -8,11 +8,73 @@ const quizRouter:Router = Router();
 quizRouter.post("/", function (request:Request, response:Response, next:NextFunction) {
     console.log(request.body);
     let level = request.body.currentLevel;
-    let maxValue = Math.pow(10,1+0.1*level);
+    let maxValue = Math.pow(10, 1 + 0.1 * level);
+    let exprList:string[] = [];
 
-    // random number between 1 and the max value
-    let targetValue = Math.round(Math.random() * (maxValue)) + 1;
+    // damage = random number between 1 and the max value
+    let targetValue = getRandomInclusive(1, maxValue);
     console.log("target val: " + targetValue);
+
+    // generate expressions and modifiers
+    let givenValue = targetValue; // starts out equal to the damage
+
+
+    // only increase the values if current given value is still smaller than the max value
+    let exprTimes = 2;
+    let modifier; // current modifier number to be added in the list
+    let limit; // max limit of modifier
+
+    for (let i = 0; i < exprTimes; i++) {
+        let randomIndex:number;
+        if (givenValue >= maxValue) {
+            randomIndex = getRandomInclusive(2, 3);
+        } else {
+            // TODO: delimit the lowest number to be 0
+            randomIndex = getRandomInclusive(0, 3);
+        }
+
+        switch (randomIndex) {
+            case 0:
+                console.log("adding...");
+                limit = maxValue - givenValue;
+                modifier = getRandomInclusive(0, limit);
+
+                givenValue = targetValue + modifier;
+                exprList.push(modifier.toString(), "-");
+                break;
+            case 1:
+                console.log("multiplying...");
+                limit = Math.floor(maxValue / givenValue);
+                modifier = getRandomInclusive(0, limit);
+
+                givenValue = targetValue * modifier;
+                exprList.push(modifier.toString(), "/");
+                break;
+            case 2:
+                console.log("subtracting...");
+                modifier = getRandomInclusive(0, givenValue);
+
+                givenValue = targetValue - modifier;
+                exprList.push(modifier.toString(), "+");
+                break;
+            case 3:
+                console.log("dividing");
+                modifier = getRandomInclusive(1, givenValue);
+
+                givenValue = targetValue / modifier;
+                exprList.push(modifier.toString(), "*");
+                break;
+            default:
+                console.log("ERROR: expression generation had some problems!");
+                break;
+        }
+    }
+
+    console.log("given val: " + givenValue);
+    console.log("list: " + exprList.toString());
+
+    // randomize the list of expressions and modifiers
+
 
     response.json({
         quiz: "test"
@@ -34,5 +96,13 @@ quizRouter.post("/check", function (request:Request, response:Response, next:Nex
         console.log(`Got error: ${e.message}`);
     });
 });
+
+
+// HELPER FUNCTIONS
+function getRandomInclusive(min:number, max:number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export { quizRouter }
