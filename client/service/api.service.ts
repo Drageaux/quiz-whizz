88c0af530a40fe0fc56c3ea5b6cf156c7766b2ad
David Thong Nguyen <thnguyen2808@gmail.com>
2016-09-09 17:21:46 -0400

    Further progress in creating a quiz

--- client/service/api.service.ts
+++ client/service/api.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import { Http, Response } from '@angular/http';
 import { Headers, RequestOptions } from '@angular/http';
 import { AuthHttp } from "angular2-jwt";
-import { Response } from "@angular/http";
 import {Observable} from "rxjs/Observable";
 
 @Injectable()
--- server/routes/quiz.ts
+++ server/routes/quiz.ts
@@ -6,79 +6,20 @@ const quizRouter:Router = Router();
 
 // get quiz question
 quizRouter.post("/", function (request:Request, response:Response, next:NextFunction) {
-    console.log(request.body);
-    let level = request.body.currentLevel;
-    let maxValue = Math.pow(10, 1 + 0.1 * level);
-    let exprList:string[] = [];
+    let level = request.body.currentLevel; // current difficulty level
+    let maxValue = Math.pow(10, 1 + 0.1 * level); // max damage user can do
 
     // damage = random number between 1 and the max value
     let targetValue = getRandomInclusive(1, maxValue);
     console.log("target val: " + targetValue);
 
     // generate expressions and modifiers
-    let givenValue = targetValue; // starts out equal to the damage
-
-
-    // only increase the values if current given value is still smaller than the max value
     let exprTimes = 2;
-    let modifier; // current modifier number to be added in the list
-    let limit; // max limit of modifier
-
-    for (let i = 0; i < exprTimes; i++) {
-        let randomIndex:number;
-        if (givenValue >= maxValue) {
-            randomIndex = getRandomInclusive(2, 3);
-        } else {
-            // TODO: delimit the lowest number to be 0
-            randomIndex = getRandomInclusive(0, 3);
-        }
-
-        switch (randomIndex) {
-            case 0:
-                console.log("adding...");
-                limit = maxValue - givenValue;
-                modifier = getRandomInclusive(0, limit);
-
-                givenValue = targetValue + modifier;
-                exprList.push(modifier.toString(), "-");
-                break;
-            case 1:
-                console.log("multiplying...");
-                limit = Math.floor(maxValue / givenValue);
-                modifier = getRandomInclusive(0, limit);
-
-                givenValue = targetValue * modifier;
-                exprList.push(modifier.toString(), "/");
-                break;
-            case 2:
-                console.log("subtracting...");
-                modifier = getRandomInclusive(0, givenValue);
-
-                givenValue = targetValue - modifier;
-                exprList.push(modifier.toString(), "+");
-                break;
-            case 3:
-                console.log("dividing");
-                modifier = getRandomInclusive(1, givenValue);
-
-                givenValue = targetValue / modifier;
-                exprList.push(modifier.toString(), "*");
-                break;
-            default:
-                console.log("ERROR: expression generation had some problems!");
-                break;
-        }
-    }
-
-    console.log("given val: " + givenValue);
-    console.log("list: " + exprList.toString());
-
-    // randomize the list of expressions and modifiers
+    let quiz = generateExpression(exprTimes, targetValue, maxValue);
+    console.log("list: " + quiz.expr.toString());
 
 
-    response.json({
-        quiz: "test"
-    });
+    response.json(quiz);
 });
 
 
@@ -98,11 +39,143 @@ quizRouter.post("/check", function (request:Request, response:Response, next:Nex
 });
 
 
-// HELPER FUNCTIONS
+//////////////////////
+// HELPER FUNCTIONS //
+//////////////////////
+/**
+ * Return a random number from min to max inclusively [min, max]
+ *
+ * @param min
+ * @param max
+ * @returns {number}
+ */
 function getRandomInclusive(min:number, max:number) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+/**
+ * Keep only allowed operations
+ *
+ * @param givenValue
+ * @param maxValue
+ */
+function filterAllowedOperators(givenValue:number, maxValue:number) {
+    let exprPossible:string[] = ["+", "*", "-", "/"];
+
+    if (givenValue >= maxValue) { // don't go over max
+        let toRemove = exprPossible.indexOf("+");
+        exprPossible.splice(toRemove, 1);
+        console.log("filter +:" + givenValue);
+    }
+    if (givenValue * 2 > maxValue) { // shouldn't be larger than half of max (to make it interesting/harder)
+        let toRemove = exprPossible.indexOf("*");
+        exprPossible.splice(toRemove, 1);
+        console.log("filter " + givenValue + "*2: " + (givenValue * 2));
+    }
+    if (givenValue <= 0) { // don't go below 0
+        let toRemove = exprPossible.indexOf("-");
+        exprPossible.splice(toRemove, 1);
+        console.log("filter -:" + givenValue);
+    }
+    if (isPrime(givenValue)) { // don't divide prime numbers; will still result in decimals but reduce weird difficult values
+        let toRemove = exprPossible.indexOf("/");
+        exprPossible.splice(toRemove, 1);
+        console.log("filter /:" + givenValue);
+    }
+    console.log("possible: " + exprPossible);
+
+    if (exprPossible.length == 0) {
+        //exprPossible.push("+")
+        // TODO: will have to test more whether it will ever hits 0 length
+    }
+    return exprPossible;
+}
+
+
+/**
+ * Return true if a prime number that's larger than 1, else false
+ *
+ * @param value
+ * @returns {boolean}
+ */
+function isPrime(value) {
+    for (var i = 2; i < value; i++) {
+        if (value % i === 0) {
+            return false;
+        }
+    }
+    return value > 1;
+}
+
+/**
+ * Loop through iterations and create an object with a list of puzzle elements and the value given to the user
+ *
+ * @param exprTimes - how many iterations should the program run through
+ * @param targetValue - the damage that user will try to get
+ * @param maxValue - the maximum value pre-determined by the difficulty
+ * @returns {string[]}
+ */
+function generateExpression(exprTimes:number, targetValue:number, maxValue:number) {
+    let result = {
+        expr: [],
+        givenValue: 0,
+        targetValue: targetValue
+    };
+    let givenValue = targetValue; // starts out equal to the damage
+    let modifier; // current modifier number to be added in the list
+    let limit; // max limit of modifier
+
+    for (let i = 0; i < exprTimes; i++) {
+        // filter only allowed expressions
+        let exprPossible:string[] = filterAllowedOperators(givenValue, maxValue);
+        console.log("possible: " + exprPossible);
+
+        let randomIndex = getRandomInclusive(0, exprPossible.length - 1);
+        switch (exprPossible[randomIndex]) {
+            case "+":
+                limit = maxValue - givenValue;
+                modifier = getRandomInclusive(0, limit);
+
+                console.log("adding.....................");
+                givenValue = targetValue + modifier;
+                result.expr.push(modifier.toString(), "-");
+                break;
+            case "*":
+                limit = Math.floor(maxValue / givenValue);
+                modifier = getRandomInclusive(1, limit);
+
+                console.log("multiplying................");
+                givenValue = targetValue * modifier;
+                result.expr.push(modifier.toString(), "/");
+                break;
+            case "-":
+                modifier = getRandomInclusive(0, givenValue);
+
+                console.log("subtracting................");
+                givenValue = targetValue - modifier;
+                result.expr.push(modifier.toString(), "+");
+                break;
+            case "/":
+                modifier = getRandomInclusive(1, givenValue);
+
+                console.log("dividing...................");
+                givenValue = targetValue / modifier;
+                result.expr.push(modifier.toString(), "*");
+                break;
+            default:
+                console.log("ERROR: expression generation had some problems!");
+                break;
+        }
+        console.log("given val: " + givenValue);
+    }
+
+    // finalize
+    // TODO: randomize the expr list before returning
+    result.givenValue = givenValue;
+
+    return result;
+}
+
 
 export { quizRouter }
