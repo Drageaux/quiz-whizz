956d1d94567addb3449dd63d5fbc6f71547c08a9
David Thong Nguyen <thnguyen2808@gmail.com>
2016-10-06 16:45:17 -0400

    Set up Auth0; not fully working; need to catch routes

--- client/service/user.service.js
+++ client/service/user.service.js
@@ -1,4 +1,4 @@
-System.register(["@angular/core", '@angular/http', "angular2-jwt", "rxjs/Observable"], function(exports_1, context_1) {
+System.register(["@angular/core", "angular2-jwt", "@angular/http", "rxjs/Observable", "./api.service"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,27 +10,41 @@ System.register(["@angular/core", '@angular/http', "angular2-jwt", "rxjs/Observa
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, http_1, angular2_jwt_1, Observable_1;
+    var core_1, angular2_jwt_1, http_1, Observable_1, api_service_1;
     var UserService;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
             },
-            function (http_1_1) {
-                http_1 = http_1_1;
-            },
             function (angular2_jwt_1_1) {
                 angular2_jwt_1 = angular2_jwt_1_1;
             },
+            function (http_1_1) {
+                http_1 = http_1_1;
+            },
             function (Observable_1_1) {
                 Observable_1 = Observable_1_1;
+            },
+            function (api_service_1_1) {
+                api_service_1 = api_service_1_1;
             }],
         execute: function() {
             UserService = (function () {
-                function UserService(authHttp, http) {
+                function UserService(authHttp, http, apiService) {
+                    var _this = this;
                     this.authHttp = authHttp;
                     this.http = http;
+                    this.apiService = apiService;
+                    // Configure Auth0
+                    this.lock = new Auth0Lock('BQBgvwL4VhCBYG5UCWCoxwBG3drfBR1A', 'davefpg.auth0.com', {});
+                    // Add callback for lock `authenticated` event
+                    this.lock.on("authenticated", function (authResult) {
+                        localStorage.setItem('id_token', authResult.idToken);
+                        var localUser = _this.getLocalUser();
+                        _this.apiService.register(localUser.name, authResult.email)
+                            .subscribe(function (data) { return console.log(data); });
+                    });
                 }
                 UserService.prototype.getLocalUser = function () {
                     var user = JSON.parse(localStorage.getItem("user"));
@@ -47,6 +61,22 @@ System.register(["@angular/core", '@angular/http', "angular2-jwt", "rxjs/Observa
                 UserService.prototype.updateLocalUser = function (user) {
                     localStorage.setItem("user", JSON.stringify(user));
                 };
+                UserService.prototype.login = function () {
+                    // Call the show method to display the widget.
+                    this.lock.show();
+                };
+                ;
+                UserService.prototype.authenticated = function () {
+                    // Check if there's an unexpired JWT
+                    // This searches for an item in localStorage with key == 'id_token'
+                    return angular2_jwt_1.tokenNotExpired();
+                };
+                ;
+                UserService.prototype.logout = function () {
+                    // Remove token from localStorage
+                    localStorage.removeItem('id_token');
+                };
+                ;
                 UserService.prototype.handleError = function (error) {
                     // In a real world app, we might use a remote logging infrastructure
                     // We'd also dig deeper into the error to get a better message
@@ -57,7 +87,7 @@ System.register(["@angular/core", '@angular/http', "angular2-jwt", "rxjs/Observa
                 };
                 UserService = __decorate([
                     core_1.Injectable(), 
-                    __metadata('design:paramtypes', [angular2_jwt_1.AuthHttp, http_1.Http])
+                    __metadata('design:paramtypes', [angular2_jwt_1.AuthHttp, http_1.Http, api_service_1.ApiService])
                 ], UserService);
                 return UserService;
             }());
--- client/service/user.service.ts
+++ client/service/user.service.ts
@@ -1,16 +1,28 @@
-import { Injectable } from "@angular/core";
-import { Http, Response } from '@angular/http';
-import { Headers, RequestOptions } from '@angular/http';
-import { AuthHttp } from "angular2-jwt";
-import { Observable } from "rxjs/Observable";
+import {Injectable} from "@angular/core";
+import {tokenNotExpired, AuthHttp} from "angular2-jwt";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {User} from "../components/user/user";
+import {ApiService} from "./api.service";
 
-import { User } from "../components/user/user";
+// Avoid name not found warnings
+declare var Auth0Lock: any;
 
 @Injectable()
 export class UserService {
+    // Configure Auth0
+    lock = new Auth0Lock('BQBgvwL4VhCBYG5UCWCoxwBG3drfBR1A', 'davefpg.auth0.com', {});
 
-    constructor(private authHttp:AuthHttp,
-                private http:Http) {
+    constructor(private authHttp: AuthHttp,
+                private http: Http,
+                private apiService: ApiService) {
+        // Add callback for lock `authenticated` event
+        this.lock.on("authenticated", (authResult) => {
+            localStorage.setItem('id_token', authResult.idToken);
+            let localUser = this.getLocalUser();
+            this.apiService.register(localUser.name, authResult.email)
+                .subscribe(data => console.log(data));
+        });
     }
 
     getLocalUser() {
@@ -25,12 +37,29 @@ export class UserService {
         }
     }
 
-    updateLocalUser(user:User) {
+    updateLocalUser(user: User) {
         localStorage.setItem("user", JSON.stringify(user));
     }
 
 
-    private handleError(error:any) {
+    public login() {
+        // Call the show method to display the widget.
+        this.lock.show();
+    };
+
+    public authenticated() {
+        // Check if there's an unexpired JWT
+        // This searches for an item in localStorage with key == 'id_token'
+        return tokenNotExpired();
+    };
+
+    public logout() {
+        // Remove token from localStorage
+        localStorage.removeItem('id_token');
+    };
+
+
+    private handleError(error: any) {
         // In a real world app, we might use a remote logging infrastructure
         // We'd also dig deeper into the error to get a better message
         let errMsg = (error.message) ? error.message :
